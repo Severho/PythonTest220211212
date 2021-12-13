cb8aa375b781f10ada52a49904c459d5181c5ed6
Sever Horghidan <dhorghidan@gmail.com>
2021-12-12 22:58:02 -0500

    updated one file

--- PythonTest/magicians.py
+++ PythonTest/magicians.py
@@ -2,5 +2,5 @@ magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
    print (magician)
    print(f"{magician.title()}, that was a great trick!")
-   #print(f"{magician.title()}, this line has been added -change 1!")
+   print(f"{magician.title()}, this line has been added -change 1!")
    print(f"I can't wait to see your next trick, {magician.title()}.\n")
